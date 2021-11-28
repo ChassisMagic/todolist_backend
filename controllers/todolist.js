bdd8a06b7e7e73f430e1c402a12a503eadac23ec
Chassismagic <kusumalingga@hotmail.com>
2021-11-28 10:06:05 +0700

    updated todolist controller function

--- controllers/todolist.js
+++ controllers/todolist.js
@@ -1,17 +1,68 @@
+const { response } = require("express");
 const Models = require("../models");
 
 module.exports = {
-  addTooList: async (req, res, next) => {
+  addTodoList: async (req, res, next) => {
     try {
-      const createTodoList = await Models.todoList.create(req.body);
-      if (createTodoList) {
-        return res.status(200).json("Created successfully!");
+      const { title, activity, completed } = req.body;
+      const addTodoList = await Models.todoList.create({
+        title,
+        activity,
+        completed
+      });
+      if (addTodoList) {
+        return res.status(200).json({msg:"Created successfully!", data:addTodoList});
       }
     } catch (error) {
-      return res.status(400).json("Created failed!");
+      return res.status(400).json({msg:"Create failed!"});
     }
   },
-  deleteTodoList: async (req, res, next) => {},
-  getAllTodolist: async (req, res, next) => {},
-  updatedTodolist: async (req, res, next) => {},
+  deleteTodoList: async (req, res, next) => {
+    try {
+      const { id } = req.body;
+      const deleteTodoList = await Models.todoList.destroy({
+        where : {
+          id
+        }
+      })
+      if (deleteTodoList) {
+        return res.status(200).json({msg: "Delete Successfully"})
+      }
+    } catch (error) {
+      return res.status(400).json({msg:"Delete failed!"});
+    }
+  },
+  getAllTodoList: async (req, res, next) => {
+    try {
+      const getAllTodoList = await Models.todoList.findAll();
+      if(getAllTodoList) {
+        return res.status(200).json({msg: "Fetch successfull", data: getAllTodoList})
+      }
+    } catch (error) {
+      return res.status(400).json({msg: "Failed to fetch data",error})
+    }
+  },
+  updateTodoList: async (req, res, next) => {
+    try {
+      const {id, title, activity, completed} = req.body;
+      const updateTodoList = await Models.todoList.update({
+        title,
+        activity,
+        completed
+      }, {
+        where: {
+          id
+        }
+      })
+
+      if (updateTodoList) {
+        return res.status(200).json({
+          msg: "Update Successful",
+          data: updateTodoList
+        })
+      } 
+    } catch (error) {
+      return res.status(400).json({msg: "Failed to update data"}, error)
+    }
+  },
 };
--- routes/todolist.js
+++ routes/todolist.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const todolistController = require("../controllers/todolist");
 
-router.post("/todolist", todolistController.addTooList);
-router.get("/todolist", todolistController.getAllTodolist);
-router.put("/todolist", todolistController.updatedTodolist);
+router.post("/todolist", todolistController.addTodoList);
+router.get("/todolist", todolistController.getAllTodoList);
+router.put("/todolist", todolistController.updateTodoList);
 router.delete("/todolist", todolistController.deleteTodoList);
 
 module.exports = router;
